947081ba93676325cd23943e44de4e79e6b6eb6c
Siesta <90094025+Pbaodoge@users.noreply.github.com>
2022-01-30 22:39:39 +0700

    Prevent using http protocol scripts
    Dunno if I'm doing this right lmao.......

---
+++ function.js
@@ -0,0 +1,20 @@
+function converthttpsindex(){
+    if(location.protocol == "http:"){
+        location.replace("https://arduinoexchange.ml");
+    }
+}
+function converthttpsabout(){
+    if(location.protocol == "http:"){
+        location.replace("https://arduinoexchange.ml/about.html");
+    }
+}
+function converthttpsapi(){
+    if(location.protocol == "http:"){
+        location.replace("https://arduinoexchange.ml/api.html");
+    }
+}
+function converthttpsdonate(){
+    if(location.protocol == "http:"){
+        location.replace("https://arduinoexchange.ml/donate.html");
+    }
+}
