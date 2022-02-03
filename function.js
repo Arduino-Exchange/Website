02fc6abe2a10459c150839c6d2a8df40dc15ed55
Pbaodoge <banggb2008@gmail.com>
2022-02-03 17:57:46 +0700

    Changed domain name

--- function.js
+++ function.js
@@ -1,20 +1,20 @@
 function converthttpsindex(){
     if(location.protocol == "http:"){
-        location.replace("https://arduinoexchange.ml");
+        location.replace("https://arduinoexchange.xyz");
     }
 }
 function converthttpsabout(){
     if(location.protocol == "http:"){
-        location.replace("https://arduinoexchange.ml/about.html");
+        location.replace("https://arduinoexchange.xyz/about.html");
     }
 }
 function converthttpsapi(){
     if(location.protocol == "http:"){
-        location.replace("https://arduinoexchange.ml/api.html");
+        location.replace("https://arduinoexchange.xyz/api.html");
     }
 }
 function converthttpsdonate(){
     if(location.protocol == "http:"){
-        location.replace("https://arduinoexchange.ml/donate.html");
+        location.replace("https://arduinoexchange.xyz/donate.html");
     }
 }
