c95f077aed816d6d316430fc61f66b0288d7b7cb
Pbaodoge <banggb2008@gmail.com>
2022-02-06 21:53:44 +0700

    Added animation on index and it is kinda cool I guess

--- function.js
+++ function.js
@@ -18,3 +18,10 @@ function converthttpsdonate(){
         location.replace("https://arduinoexchange.xyz/donate.html");
     }
 }
+//script to copy addresses to clipboard
+function clipboard() {
+    var copyText = document.getElementById("clipboard");
+    copyText.select();
+    copyText.setSelectionRange(0, 10); 
+    navigator.clipboard.writeText(copyText.value);
+  }
